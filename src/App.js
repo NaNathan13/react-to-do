caca0fb03df336936d7fa1911d44983137e3d176
NaNathan13 <natewilson135@gmail.com>
2019-02-12 13:56:29 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -38,25 +38,29 @@ class App extends Component {
    render() {
     return (
       <div className="App">
-        <ul>
-        { this.state.todos.map( (todo, index) => 
-          <ToDo 
-          key={ index } 
-          description={ todo.description } 
-          isCompleted={ todo.isCompleted } 
-          toggleComplete={ () => this.toggleComplete(index) } 
-          deleteTodo= { () => this.deleteTodo(index) }
-          />
-          )}
-        </ul>
-        <form onSubmit={ (e) => this.handleSubmit(e) }>
-          <input 
-          type="text" 
-          value={ this.state.newTodoDescription } 
-          onChange={ (e) => this.handleChange(e) }
-          />
-          <input type="submit"/>
-        </form>
+        <div className="todo-wrapper">
+          <h1 className="header">To Do List</h1>
+          <ul className="to-dos">
+          { this.state.todos.map( (todo, index) => 
+            <ToDo 
+            key={ index } 
+            description={ todo.description } 
+            isCompleted={ todo.isCompleted } 
+            toggleComplete={ () => this.toggleComplete(index) } 
+            deleteTodo= { () => this.deleteTodo(index) }
+            />
+            )}
+          </ul>
+          <form onSubmit={ (e) => this.handleSubmit(e) }>
+            <input 
+            className="input"
+            type="text" 
+            value={ this.state.newTodoDescription } 
+            onChange={ (e) => this.handleChange(e) }
+            />
+            <button className="btn">Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
+import '../App.css';
 
 class ToDo extends Component {
     render () {
         return (
             <div>
-                <li>
+                <li className="to-dos">
                     <input type="checkbox" checked={ this.props.isCompleted } onChange={this.props.toggleComplete} />
                     <span>{ this.props.description }</span>
-                    <button onClick={this.props.deleteTodo}>X</button>
+                    <button className="x-box" onClick={this.props.deleteTodo}>X</button>
                 </li>
                 
             </div>
